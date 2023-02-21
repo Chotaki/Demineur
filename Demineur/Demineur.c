6c11a38c4ecf7cfa6e7368c4aa8e3e450da39c65
anaelle <anaelle.romanzin@gmail.com>
2023-02-21 11:32:25 +0100

    creuser

--- Demineur/Demineur.c
+++ Demineur/Demineur.c
@@ -2,48 +2,192 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void rayan(char*);
+void rayan(int *);
+void loris(char**, int*);
+void delay(int);
+void antoine(char**);
+void vianney(char**, int*, int);
 
 int main()
 {
     int i;
     int j;
-    char* tableau[100];
+    char* tableau_jeu[100];
+    int tableau_bombe[18];
+    int choix;
 
     for (i = 0; i < 100; i++) 
     {
-        tableau[i] = "[_]"; 
+        tableau_jeu[i] = "[_]"; 
     }
 
-    rayan(&tableau);
+    rayan(tableau_bombe);
 
-    for (j = 0; j < 10; j++)
+    for (i = 0; i < 100; i++)
     {
-        for (i = 0; i < 10; i++)
+        for (j = 0; j < 10; j++)
         {
-            printf("%s", tableau[j*10+i]);
+            for (i = 0; i < 10; i++)
+            {
+                printf("%s", tableau_jeu[j*10+i]);
+            }
+            printf("\n");
         }
-        printf("\n");
+
+        for (i = 0; i < 19; i++)
+        {
+            printf("%d\n", tableau_bombe[i]);
+        }
+
+        printf("Choisissez 0:creuser , 1:poser drapeau");
+        scanf_s("%d", &choix);
+        if (choix == 0) {
+            loris(tableau_jeu, tableau_bombe);
+        }
+        if (choix == 1) {
+            antoine(tableau_jeu);
+        }
+
+        system("cls");
     }
 
     return 0;
 }
 
-void rayan(char* *tableau)
+void rayan(int *tableau_bombe)
 {
-   tableau[9] = "[X]";
-   tableau[25] = "[X]";
-   tableau[96] = "[X]";
-   tableau[14] = "[X]";
-   tableau[5] = "[X]";
-   tableau[85] = "[X]";
-   tableau[69] = "[X]";
-   tableau[59] = "[X]";
-   tableau[7] = "[X]";
-   tableau[55] = "[X]";
+    int indice = 0;
+    srand(time(NULL));
+
+    for (int i = 0; i < 19; i++)
+    {
+        indice = rand() % (100);
+
+        tableau_bombe[i] = indice;
+    }
 }
 
+void loris(char* *tableau_jeu, int *tableau_bombe)
+{
+    int i;
+    int j;
+    int indice;
+    printf("Choisissez une case a creuser entre 0 et 99 :");
+    scanf_s("%d", &indice);
+
+    for (i = 0; i < 19; i++)
+    { 
+        if (tableau_bombe[i] == indice) {
+            j = i;
+        }
+    }
+
+    if (tableau_bombe[j] == indice)
+    {
+        printf("PERDU");
+        delay(1);
+        abort();
+    }
+    else {
+        vianney(tableau_jeu,tableau_bombe,indice);
+
+    }
+}
+
+void antoine(char* *tableau_jeu)
+{
+    int indice;
+    printf("Choisissez une case ou poser un drapeau entre 0 et 99 :");
+    scanf_s("%d", &indice);
+
+    tableau_jeu[indice] = "[4]";
+}
+
+void vianney(char* *tableau_jeu, int* tableau_bombe, int indice)
+{
+    int j1;
+    int j2;
+    int j3;
+    int j4;
+    int j5;
+    int j6;
+    int j7;
+    int j8;
+
+    for (int i = 0; i < 19; i++)
+    {
+        if (tableau_bombe[i] == indice - 11)
+        {
+            j1 = 1;
+        }
+        if (tableau_bombe[i] == indice - 10)
+        {
+            j2 = 1;
+        }
+        if (tableau_bombe[i] == indice - 9)
+        {
+            j3 = 1;
+        }
+        if (tableau_bombe[i] == indice - 1)
+        {
+            j4 = 1;
+        }
+        if (tableau_bombe[i] == indice + 1)
+        {
+            j5 = 1;
+        }
+        if (tableau_bombe[i] == indice + 9)
+        {
+            j6 = 1;
+        }
+        if (tableau_bombe[i] == indice + 10)
+        {
+            j7 = 1;
+        }
+        if (tableau_bombe[i] == indice + 11)
+        {
+            j8 = 1;
+        }
+
+    }
+    if (j1 == 0) {
+        tableau_jeu[indice - 11] = "[0]";
+    }
+    if (j2 == 0) {
+        tableau_jeu[indice - 10] = "[0]";
+    }
+    if (j3 == 0) {
+        tableau_jeu[indice - 9] = "[0]";
+    }
+    if (j4 == 0) {
+        tableau_jeu[indice - 1] = "[0]";
+    }
+    if (j5 == 0) {
+        tableau_jeu[indice + 1] = "[0]";
+    }
+    if (j6 == 0) {
+        tableau_jeu[indice + 9] = "[0]";
+    }
+    if (j7 == 0) {
+        tableau_jeu[indice + 10] = "[0]";
+    }
+    if (j8 == 0) {
+        tableau_jeu[indice + 11] = "[0]";
+    }
+    
+
+}
+
+void delay(int temps)
+{
+    int seconds = 1000 * temps;
+    clock_t start = clock();
+
+    while (clock() < start + seconds);
+}
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage

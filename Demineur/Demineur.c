6ba5b0dd1887c7ed46c344c95688ba3a92ff0c70
anaelle <anaelle.romanzin@gmail.com>
2023-02-24 15:27:43 +0100

    passage en 2D

--- Demineur/Demineur.c
+++ Demineur/Demineur.c
@@ -6,98 +6,124 @@
 #include <time.h>
 #include <string.h>
 
-void rayan(int *);
-void loris(char*, int*);
+#define BOMB_COUNT 18
+
+#define SIZE 10
+#define TOTAL_SIZE (SIZE * SIZE)
+
+typedef struct Point Point;
+
+void random_bomb(Point[BOMB_COUNT]);
+void dig(char*, Point[BOMB_COUNT]);
 void delay(int);
-void antoine(char*);
-void vianney(char*, int*, int);
-int william(char*, int*, int);
-void victor(char*);
-void lilian(char*, int*, int);
-int danny(int*, int);
+void flag(char*);
+//void link(char*, int*, int);
+//int bomb_around(char*, int*, int);
+void win(char*);
+//void dig_around(char*, int*, int);
+int double_finder(Point[BOMB_COUNT], int, int);
+int getIndex2D(int, int);
+void getIndex1D(int, int *, int *);
+
+
+struct Point
+{
+    int x;
+    int y;
+};
+
 
 int main()
 {
     int i;
     int j;
-    char tableau_jeu[100];
-    int tableau_bombe[18] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    char tableau_jeu[TOTAL_SIZE];
+    Point tableau_bombe[BOMB_COUNT];
     int choix;
 
-    for (i = 0; i < 100; i++) 
+    random_bomb(tableau_bombe);
+
+    for (i = 0; i < TOTAL_SIZE; i++)
     {
         tableau_jeu[i] = '_';
     }
 
-    rayan(tableau_bombe);
-
-    for (i = 0; i < 100; i++)
+    while(1)
     {
-        for (j = 0; j < 10; j++)
+        for (j = 0; j < SIZE; j++)
         {
-            for (i = 0; i < 10; i++)
+            for (i = 0; i < SIZE; i++)
             {
-                printf("[%c]", tableau_jeu[j*10+i]);
+                printf("[%c]", tableau_jeu[j* SIZE +i]);
             }
             printf("\n");
         }
 
-        for (i = 0; i < 18; i++)
+        for (i = 0; i < BOMB_COUNT; i++)
         {
-            printf("%d\n", tableau_bombe[i]);
+            printf(" %d %d\n", tableau_bombe[i].x, tableau_bombe[i].y);
         }
 
         printf("Choisissez 0:creuser , 1:poser drapeau");
         scanf_s("%d", &choix);
         if (choix == 0) {
-            loris(tableau_jeu, tableau_bombe);
+            dig(tableau_jeu, tableau_bombe);
         }
         if (choix == 1) {
-            antoine(tableau_jeu);
+            flag(tableau_jeu);
         }
 
-        victor(tableau_jeu);
+        win(tableau_jeu);
         system("cls");
     }
 
     return 0;
 }
 
-void rayan(int *tableau_bombe)
+int getIndex2D(int x, int y)
 {
-    int indice = 0;
-    int x = 0;
-    int y = 0;
+    return x * SIZE + y;
+}
+
+void getIndex1D(int i, int * x, int * y)
+{
+    *x = i / SIZE;
+    *y = i % SIZE;
+}
+
+void random_bomb(Point tableau_bombe[BOMB_COUNT])
+{
+    int a = 0;
+    int b = 0;
     srand(time(NULL));
 
-    for (int i = 0; i < 19; i++) {
-
-        if (tableau_bombe[i] == 0) {
-            x++;
-        }
+    for (int i = 0; i < BOMB_COUNT; i++) {
+        a++;
     }
 
-    while (x > 0) {
+    while (a > 0) {
 
-        indice = rand() % (100);
+        int indice_x = rand() % 10;
+        int indice_y = rand() % 10;
 
-        if (danny(tableau_bombe, indice) == 0) {
-            tableau_bombe[y] = indice;
-            x = x - 1;
-            y++;
+        if (double_finder(tableau_bombe, indice_x, indice_y) == 0) {
+            tableau_bombe[b].x = indice_x;
+            tableau_bombe[b].y = indice_y;
+            a = a - 1;
+            b++;
         }
     }
     
 
 }
 
-int danny(int* tableau_bombe, int indice) {
+int double_finder(Point tableau_bombe[BOMB_COUNT], int indice_x, int indice_y) {
 
     int x = 0;
 
-    for (int i = 0; i < 19; i++) {
+    for (int i = 0; i < BOMB_COUNT; i++) {
         
-        if (tableau_bombe[i] == indice) {
+        if (tableau_bombe[i].x == indice_x && tableau_bombe[i].y == indice_y) {
             x++;
         }
     }
@@ -110,32 +136,29 @@ int danny(int* tableau_bombe, int indice) {
     }
 }
 
-void loris(char *tableau_jeu, int *tableau_bombe)
+void dig(char* tableau_jeu, Point tableau_bombe[BOMB_COUNT])
 {
     int i;
-    int j = 0;
-    int indice;
-    printf("Choisissez une case a creuser entre 0 et 99 :");
-    scanf_s("%d", &indice);
+    int indice_x;
+    int indice_y;
+    printf("Choisissez une ligne ou jouer:");
+    scanf_s("%d", &indice_x);
+    printf("Choisissez une colone ou jouer:");
+    scanf_s("%d", &indice_y);
 
     for (i = 0; i < 19; i++)
     { 
-        if (tableau_bombe[i] == indice) {
-            j = i;
+        if (tableau_bombe[i].x == indice_x && tableau_bombe[i].y == indice_y) {
+            printf("PERDU");
+            delay(1);
+            abort();
         }
     }
 
-    if (tableau_bombe[j] == indice) {
-        printf("PERDU");
-        delay(1);
-        abort();
-    }
-    else {
-        vianney(tableau_jeu,tableau_bombe,indice);
-    }
+    //link(tableau_jeu, tableau_bombe, indice);
 }
 
-void antoine(char *tableau_jeu)
+void flag(char *tableau_jeu)
 {
     int indice;
     printf("Choisissez une case ou poser un drapeau entre 0 et 99 :");
@@ -144,81 +167,97 @@ void antoine(char *tableau_jeu)
     tableau_jeu[indice] = 'P';
 }
 
-void vianney(char *tableau_jeu, int* tableau_bombe, int indice)
+/*void link(char* tableau_jeu, int* tableau_bombe, int indice)
 {
-    if (william(tableau_jeu, tableau_bombe, indice) == 0  && (indice + 1) % 10 != 0) {
-        william(tableau_jeu, tableau_bombe, indice);
-        lilian(tableau_jeu, tableau_bombe, indice);
+    if (bomb_around(tableau_jeu, tableau_bombe, indice) == 0) {
+        bomb_around(tableau_jeu, tableau_bombe, indice);
+        dig_around(tableau_jeu, tableau_bombe, indice);
     }
 
 }
 
-void lilian(char* tableau_jeu, int* tableau_bombe, int indice)
+void dig_around(char* tableau_jeu, int* tableau_bombe, int indice)
 {
     if ((indice + 1) % 10 != 0 && indice % 10 != 0) {
-        william(tableau_jeu, tableau_bombe, indice - 11);
-        william(tableau_jeu, tableau_bombe, indice - 10);
-        william(tableau_jeu, tableau_bombe, indice - 9);
-        william(tableau_jeu, tableau_bombe, indice - 1);
-        william(tableau_jeu, tableau_bombe, indice + 1);
-        william(tableau_jeu, tableau_bombe, indice + 9);
-        william(tableau_jeu, tableau_bombe, indice + 10);
-        william(tableau_jeu, tableau_bombe, indice + 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 11);
     }
 
     if ((indice + 1) % 10 == 0) {
-        william(tableau_jeu, tableau_bombe, indice - 11);
-        william(tableau_jeu, tableau_bombe, indice - 10);
-        william(tableau_jeu, tableau_bombe, indice - 1);
-        william(tableau_jeu, tableau_bombe, indice + 9);
-        william(tableau_jeu, tableau_bombe, indice + 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 10);
     }
 
     if (indice % 10 == 0) {
-        william(tableau_jeu, tableau_bombe, indice - 10);
-        william(tableau_jeu, tableau_bombe, indice - 9);
-        william(tableau_jeu, tableau_bombe, indice + 1);
-        william(tableau_jeu, tableau_bombe, indice + 10);
-        william(tableau_jeu, tableau_bombe, indice + 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 11);
+    }
+
+    if (indice % 90 < 10) {
+        bomb_around(tableau_jeu, tableau_bombe, indice - 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 1);
+    }
+
+    if (indice < 10) {
+        bomb_around(tableau_jeu, tableau_bombe, indice - 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 11);
     }
 
 
-        /*if (william(tableau_jeu, tableau_bombe, indice - 11) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice - 11);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice - 11) == 0) {
+            link(tableau_jeu, tableau_bombe, indice - 11);
         }
-        if (william(tableau_jeu, tableau_bombe, indice - 10) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice - 10);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice - 10) == 0) {
+            link(tableau_jeu, tableau_bombe, indice - 10);
         }
-        if (william(tableau_jeu, tableau_bombe, indice - 9) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice - 9);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice - 9) == 0) {
+            link(tableau_jeu, tableau_bombe, indice - 9);
         }
-        if (william(tableau_jeu, tableau_bombe, indice - 1) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice - 1);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice - 1) == 0) {
+            link(tableau_jeu, tableau_bombe, indice - 1);
         }
-        if (william(tableau_jeu, tableau_bombe, indice + 1) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice + 1);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice + 1) == 0) {
+            link(tableau_jeu, tableau_bombe, indice + 1);
         }
-        if (william(tableau_jeu, tableau_bombe, indice + 9) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice + 9);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice + 9) == 0) {
+            link(tableau_jeu, tableau_bombe, indice + 9);
         }
-        if (william(tableau_jeu, tableau_bombe, indice + 10) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice + 10);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice + 10) == 0) {
+            link(tableau_jeu, tableau_bombe, indice + 10);
         }
-        if (william(tableau_jeu, tableau_bombe, indice + 11) == 0) {
-            vianney(tableau_jeu, tableau_bombe, indice + 11);
+        if (bomb_around(tableau_jeu, tableau_bombe, indice + 11) == 0) {
+            link(tableau_jeu, tableau_bombe, indice + 11);
         }
-        william(tableau_jeu, tableau_bombe, indice - 11);
-        william(tableau_jeu, tableau_bombe, indice - 10);
-        william(tableau_jeu, tableau_bombe, indice - 9);
-        william(tableau_jeu, tableau_bombe, indice - 1);
-        william(tableau_jeu, tableau_bombe, indice + 1);
-        william(tableau_jeu, tableau_bombe, indice + 9);
-        william(tableau_jeu, tableau_bombe, indice + 10);
-        william(tableau_jeu, tableau_bombe, indice + 11);*/
+        bomb_around(tableau_jeu, tableau_bombe, indice - 11);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice - 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 1);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 9);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 10);
+        bomb_around(tableau_jeu, tableau_bombe, indice + 11);
 }
 
 
-int william(char *tableau_jeu, int *tableau_bombe, int indice)
+int bomb_around(char *tableau_jeu, int *tableau_bombe, int indice)
 {
     int nb_bombe = 0;
     char int_str[2];
@@ -295,22 +334,23 @@ int william(char *tableau_jeu, int *tableau_bombe, int indice)
         }
     }
 
+
     sprintf_s(int_str, 2, "%d", nb_bombe);
     tableau_jeu[indice] = int_str[0];
     return nb_bombe;
-}
+}*/
 
-void victor(char* tableau_jeu)
+void win(char* tableau_jeu)
 {
     int nb_cases = 0;
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < TOTAL_SIZE; i++)
     {
         if (tableau_jeu[i] != '_') {
             nb_cases++;
         }
     }
-    if (nb_cases == 100) {
+    if (nb_cases == TOTAL_SIZE) {
         printf("VICTOIRE !");
         delay(1);
         abort();
@@ -324,14 +364,3 @@ void delay(int temps)
 
     while (clock() < start + seconds);
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
